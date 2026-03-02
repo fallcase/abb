64809fd77ec0aec61c373fb5260ab5fc921d465b
r4vesky <r4vesky@gmail.com>
2026-03-03 03:44:08 +0700

    fix cors list

---
+++ assets/index-ByIT-geC.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-BLHQ0bt_.js";import"./index-DrFsxgwR.js";var d=a;export{d as default};
---
+++ assets/src-UW24ZMRV-CM6G83YZ.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-BLHQ0bt_.js";import"./index-DrFsxgwR.js";var t=a;export{t as default};
