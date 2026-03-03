6a1bcb0c0207952da6378f2820fa7197548a3aa3
r4vesky <r4vesky@gmail.com>
2026-03-03 20:39:00 +0700

    fix cors list

--- assets/src-UW24ZMRV-BHJRXQBG.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-DcXR9NJ-.js";import"./index-B5j5NEDZ.js";var t=a;export{t as default};
---
+++ assets/src-UW24ZMRV-D5pBIcTx.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-_fXJ_rdD.js";import"./index-DSt6Kg9_.js";var t=a;export{t as default};
