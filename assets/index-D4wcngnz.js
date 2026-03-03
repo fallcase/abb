5238df568ffea4413b6e13671a091dd52d9f3974
r4vesky <r4vesky@gmail.com>
2026-03-03 20:34:39 +0700

    fix cors list

--- assets/index-D4wcngnz.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-BlBQhMwU.js";import"./index-CgvhCJy1.js";var d=a;export{d as default};
---
+++ assets/src-UW24ZMRV-BHJRXQBG.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-DcXR9NJ-.js";import"./index-B5j5NEDZ.js";var t=a;export{t as default};
