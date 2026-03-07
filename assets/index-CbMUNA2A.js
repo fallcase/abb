9705a63a4c1d7248a5dc8272a4735e34676517a6
r4vesky <r4vesky@gmail.com>
2026-03-07 13:07:58 +0700

    add platega

--- assets/index-CbMUNA2A.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-BhEoY_rT.js";import"./index-8YTMJsOb.js";var d=a;export{d as default};
---
+++ assets/src-UW24ZMRV-DWB66z-p.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-CsEHiV-0.js";import"./index-CtzGOGJv.js";var t=a;export{t as default};
