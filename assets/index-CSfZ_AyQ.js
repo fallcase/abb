64cb5919ec9ced4bc553b07a521410cea51d00bd
r4vesky <r4vesky@gmail.com>
2026-03-11 03:24:15 +0700

    add platega

---
+++ assets/index-CSfZ_AyQ.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-CAUv2YI-.js";import"./index-Comt4Fqw.js";var d=a;export{d as default};
--- assets/src-UW24ZMRV-DI4JKaI6.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-6R-SuqF9.js";import"./index-CHuK_93X.js";var t=a;export{t as default};
